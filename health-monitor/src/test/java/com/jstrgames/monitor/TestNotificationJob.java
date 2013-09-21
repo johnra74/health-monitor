e529624a86848b785d13052d4be40840a7fe9b42
ra <ra@jstrgames.com>
2013-09-20 21:08:57 -0400

    added execute test

--- health-monitor/src/test/java/com/jstrgames/monitor/TestNotificationJob.java
+++ health-monitor/src/test/java/com/jstrgames/monitor/TestNotificationJob.java
@@ -10,17 +10,73 @@ import java.util.Properties;
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 
 import org.junit.Test;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.jstrgames.monitor.cfg.ConfigurationException;
 import com.jstrgames.monitor.cfg.NotificationConfig;
 
 public class TestNotificationJob {
 	private final static Logger LOG = LoggerFactory.getLogger(TestNotificationJob.class);
+		
+	@Test
+	public void testExecute() {
+		JobExecutionContext context = mock(JobExecutionContext.class);
+		NotificationConfig notifyCfg = mock(NotificationConfig.class);
+		JobDataMap jobDataMap = mock(JobDataMap.class);
+		JobManager jobMgr = mock(JobManager.class);
+		Properties prop = mock(Properties.class);
+		Message msg = mock(Message.class);
+		
+		when(context.getMergedJobDataMap()).thenReturn(jobDataMap);
+		when(jobDataMap.get("notifyCfg")).thenReturn(notifyCfg);
+		when(jobDataMap.get("jobMgr")).thenReturn(jobMgr);
+		when(notifyCfg.getMailProperty()).thenReturn(prop);
+		when(notifyCfg.shouldSendNotification(jobMgr)).thenReturn(true);
+		when(notifyCfg.getMailProperty()).thenReturn(prop);
+		when(notifyCfg.isSmtpAuthEnabled()).thenReturn(false);
+		
+		try {
+			when(notifyCfg.createMessage(any(Session.class), eq(jobMgr))).thenReturn(msg);
+			Address[] recipients = new Address[1];
+			Address[] mailfrom = new Address[1];
+			recipients[0] = new InternetAddress("test@localhost", "test");
+			mailfrom[0] = new InternetAddress("test@localhost", "test");	
+			when(msg.getAllRecipients()).thenReturn(recipients);
+			when(msg.getFrom()).thenReturn(mailfrom);
+			when(msg.getSubject()).thenReturn("test");
+			when(msg.getContent()).thenReturn("testing 1... 2... 3...");
+			when(msg.getContentType()).thenReturn("text/plain");	
 			
+		} catch (ConfigurationException e) {
+			fail("failed to setup mock message");
+		} catch (UnsupportedEncodingException e) {
+			fail("failed to setup mock address");
+		} catch (MessagingException e) {
+			fail("failed to setup mock message body");
+		} catch (IOException e) {
+			fail("failed to setup mock connection");
+		}
+
+		when(prop.get("mail.smtp.starttls.enable")).thenReturn(false);
+		when(prop.get("mail.smtp.host")).thenReturn("localhost");
+		when(prop.get("mail.smtp.port")).thenReturn(25);
+		
+		NotificationJob job = new NotificationJob();
+		try {
+			job.execute(context);
+		} catch (JobExecutionException e) {
+			fail("failed to execute job with mock context!");
+		}
+	}
+	
 	@Test
 	public void testDontSendMail() {
 		NotificationConfig cfg = mock(NotificationConfig.class);
